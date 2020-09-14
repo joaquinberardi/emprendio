a7a1c617b47e4bf7c930162369af564f47057cdc
Melanie <melgiacchetta@outlook.com>
2020-09-14 11:10:12 -0300

    c

--- emprendio/routes/emprendedores.js
+++ emprendio/routes/emprendedores.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 var emprendedoresControllers = require('../controllers/emprendedoresControllers')
 
-<<<<<<< Updated upstream
-router.get('/', emprendedoresControllers.perfiles);
-=======
 
->>>>>>> Stashed changes
+router.get('/', emprendedoresControllers.perfiles);
 router.get('/agregarpost', emprendedoresControllers.agregarPost); // LISTO *(href)
 router.get('/perfiles', emprendedoresControllers.perfiles); // LISTO *(href)
 router.get('/miperfil', emprendedoresControllers.miperfil); // LISTO *(href)
