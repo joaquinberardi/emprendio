813b243f64b38c05e37f458045e1da43f06c8239
Michael Dunn <mdunn@udesa.edu.ar>
2020-09-23 10:15:53 -0300

    fotis

--- emprendio/routes/emprendedores.js
+++ emprendio/routes/emprendedores.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var emprendedoresControllers = require('../controllers/emprendedoresControllers')
 
 
-router.get('/', emprendedoresControllers.perfilesVarios);
+router.get('/', emprendedoresControllers.perfiles);
 router.get('/agregarpost', emprendedoresControllers.agregarPost); // LISTO 
 router.get('/miperfil', emprendedoresControllers.miperfil); // LISTO
 router.get('/perfilesVarios', emprendedoresControllers.perfiles); // LISTO *(href)
